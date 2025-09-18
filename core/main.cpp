5f6cc9ce1af6075afad89f4257814b551b8b3d62
MISTICMAN23 <132823841+723mist@users.noreply.github.com>
2025-09-18 21:24:33 +0300

    Add full directory

---
+++ core/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "src/window/window.hpp"
+
+int main() {
+    EngineWindow window;
+    window.Init();
+    return 0;
+}
---
+++ core/src/window/window.cpp
@@ -0,0 +1,55 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "window.hpp"
+#include <iostream>
+#include <string>
+//#include <sstream>
+
+int EngineWindow::Init(const char* title, int width, int height){
+
+    if (!glfwInit()){
+        std::cerr << "Error init window" << std::endl;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+
+    if (!window){
+        std::cerr << "Error create window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    //glfwSwapInterval(1); //VSync
+
+    static unsigned frameCount = 0;
+    double lastTime = glfwGetTime();
+    int frames = 0;
+
+    GLFWcursor* cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+    glfwSetCursor(window, cursor);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        frames++;
+        double currentTime = glfwGetTime();
+        if (currentTime - lastTime >= 1.0) {
+            std::cout << "FPS: " << frames << std::endl;
+            std::string titleWithFPS = std::string(title) + " FPS: " + std::to_string(int(frames));
+            glfwSetWindowTitle(window, titleWithFPS.c_str());
+            frames = 0;
+            lastTime = currentTime;
+        }
+
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
