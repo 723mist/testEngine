8fdb8094eb586468199c4801c3a4359e20d4a436
MISTICMAN23 <132823841+723mist@users.noreply.github.com>
2025-09-18 21:27:53 +0300

    18.09.2025 21:27 МСК

---
+++ core/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "window/window.hpp"
+
+int main() {
+    EngineWindow window;
+    window.Init();
+    return 0;
+}
---
+++ core/window/window.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class EngineWindow{
+public:
+    int Init(const char* title = "VEngine window 500x500", int width = 500, int height = 500);
+};
